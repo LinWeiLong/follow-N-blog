eb05c93df9a25b64d491e91fcea012ba7a2aefe3
LinWeiLong <linwl@xinli001.com>
2017-08-03 00:18:24 +0800

    完成N-blog这个项目。除了测试和部署环节

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.redirect('/posts');
+        // res.render('index')
     });
+
+
     app.use('/signup', require('./signup.js'));
     app.use('/signin', require('./signin.js'));
     app.use('/signout', require('./signout.js'));
     app.use('/posts', require('./posts.js'));
+    // 404 page
+    // 放到最下面，是因为路由规则是按顺序来的，放在上面，就会直接进去404了
+    app.use(function (req, res) {
+        if (!res.headersSent) {
+            res.status(404).render('404');
+        }
+    });
+    app.use(function (err, req, res, next) {
+        res.render('error', {
+            error: err
+        });
+    });
 };
